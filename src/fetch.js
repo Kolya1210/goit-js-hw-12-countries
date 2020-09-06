7320a6b714aa303c8c58746994cd17043f6a3262
Mykola Babytskyi <zveryok12101994@gamil.com>
2020-09-06 19:03:31 +0300

    HW1

---
+++ src/fetch.js
@@ -0,0 +1,14 @@
+const baseUrl = 'https://restcountries.eu/rest/v2/name/'
+
+
+const fetchCountries = function (searchQuery) {
+    return fetch(baseUrl + searchQuery)
+    .then((response) => {
+    if (response.ok) {
+    return response.json();
+    }
+    throw new Error(response.status);
+    })
+    .catch((err) => console.log(err));
+    };
+export default fetchCountries
--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 import './styles.css';
+
+import {debounce} from 'lodash'
+import fetch from './fetch.js';
+import country from './handlebar/item.hbs'
+import list from './handlebar/item.hbs'
+import {error} from '@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+const inputText = document.querySelector('#input');
+const itemText = document.querySelector('#box');
+
+inputText.addEventListener('input', debounce(sergCountry, 1000));
+
+
+function sergCountry() {
+    const searchValue = input.value;
+    if (searchValue === ''){
+        return;
+    }
+    fetch(searchValue).then((data) => {
+        if (!data) {
+            error({ text: "Not found" });
+            return;
+            }
+        if (data.length !== 1 && data.length<= 10  ){
+        list(data)
+        itemText.insertAdjacentHTML('beforeend', list(data))
+    }
+    if(data.length === 1){
+       country(data);
+       itemText.insertAdjacentHTML('beforeend', country(data))
+    }
+    if(data.length> 10) {
+            error({text: 'To many requests'})
+        }
+    })
+    if (searchValue === '') {
+        itemText.innerHTML = '';
+    }
+}
